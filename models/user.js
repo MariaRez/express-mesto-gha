615820a0e629e4184cfc2c6972d84aa0ad1f3bfc
MariaRez <maria.v.reznik@yandex.ru>
2022-11-17 03:13:18 +0400

    feat: add email and password to userSchema

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const validator = require('validator');
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({ // схема карточки
@@ -14,6 +15,21 @@ const userSchema = new mongoose.Schema({ // схема карточки
     required: true,
   },
   avatar: { // ссылка на аватарку, строка, обязательное поле
+    type: String,
+    required: true,
+  },
+  email: { // почта пользователя, уникальное значение
+    type: String,
+    required: true,
+    unique: true,
+    validate(value) { // валидация для почты пользователя
+      // в случае передачи не подходящих данных будет создана ошибка
+      if (!validator.isEmail(value)) {
+        throw new Error('Передан некорректный формат электронной почты');
+      }
+    },
+  },
+  password: { // пароль пользователя для входа
     type: String,
     required: true,
   },
